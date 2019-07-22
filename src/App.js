89b3a619a357924ef591fb56cda3f1066f2fad9f
Michael Harris <mhguitar1997@gmail.com>
2019-07-22 14:16:47 -0700

    CardContext provider wrap all components

--- src/App.js
+++ src/App.js
@@ -20,10 +20,12 @@ function App() {
   return (
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
-        {/* Routes */}
-        <Route exact path="/" component={Products} />
-        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+        <CardContext.Provider value={cart}>
+          <Navigation cart={cart} />
+          {/* Routes */}
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+        </CardContext.Provider>
       </ProductContext.Provider>
     </div>
   );
