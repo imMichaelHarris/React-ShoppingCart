985217313ee689fc7902424a98a902491d588b9c
Michael Harris <mhguitar1997@gmail.com>
2019-07-22 14:47:12 -0700

    Remove props in shopping cart component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import data from "./data";
 import { ProductContext } from "./contexts/ProductContext";
-import { CardContext } from "./contexts/CartContext;";
+import { CartContext } from "./contexts/CartContext";
 
 // Components
 import Navigation from "./components/Navigation";
@@ -20,12 +20,12 @@ function App() {
   return (
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <CardContext.Provider value={cart}>
+        <CartContext.Provider value={cart}>
           <Navigation />
           {/* Routes */}
           <Route exact path="/" component={Products} />
           <Route path="/cart" component={ShoppingCart} />
-        </CardContext.Provider>
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
